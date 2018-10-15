384c90bb6c4447171215b266b240ebaf6bce793c
nantapat <minibio_nan@hotmail.com>
2018-10-15 17:05:20 +0700

    Change UI design of these pages

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Card, CardBody, CardGroup, Col, Container, FormGroup, Label, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
-
+import { Button, Card, CardBody, CardGroup, Col, Container, FormGroup, Label, Input, InputGroup, InputGroupAddon, InputGroupText, Row, ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import {Nav, Navbar, NavDropdown, MenuItem, Tabs, ButtonToolbar, Table, ButtonGroup, Grid, Panel,  FormControl, DropdownButton} from 'react-bootstrap';
 class Login extends Component {
   render() {
     return (
       <div className="app flex-row align-items-center">
         <Container>
           <Row className="justify-content-center">
-            <Col md="6">
-              <Card className="mx-4">
+            <Col md="6" className="LoginPage">
+              <Card className="mx-4 Login">
                 <CardBody className="p-4">
                   <h1 className='text-center color-main'>Login</h1>
                   <p className="text-center color-main">Sign In to your account</p>
                   <FormGroup>
                     <Label htmlFor="email">Email</Label>
-                    <Input type="email" id="email" placeholder="email" required />
+                    <Input type="email" id="email" placeholder="petreamihaic@gmail.com" required />
                   </FormGroup>
                   <FormGroup>
                     <Label htmlFor="password">Password</Label>
-                    <Input type="password" id="password" placeholder="password" required />
+                    <Input type="password" id="password" placeholder="●●●●●●●" required />
                   </FormGroup>
                   
                   <Row style={{ marginTop: -20 }}>
@@ -31,21 +31,21 @@ class Login extends Component {
                   <Row className="justify-content-center">
                     <Col md='auto'>
                       <Link to='/dashboard'>
-                        <Button color="primary" className="px-4">Login</Button>
+                        <Button color="primary" className="px-4 Button-Login">LOGIN</Button>
                       </Link>
-                    </Col>
-                  </Row>
-                  <br/>
-                  <Row className="justify-content-center">
-                    <Col md='auto'>
-                      No account? 
-                      <Link to='/register'> Signup Now!</Link>
                     </Col>
                   </Row>
                 </CardBody>
               </Card>
+              <Row className="justify-content-center Link-to-signup">
+                <Col md='auto'>
+                  Don’t have an account? 
+                  <Link to='/register'>  Signup!</Link>
+                </Col>
+              </Row>
             </Col>
           </Row>
+     
         </Container>
       </div>
     );
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,33 +1,71 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Card, CardBody, CardFooter, Col, Container, FormGroup, Label, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import { Button, Card, CardBody, CardGroup, Col, Container, FormGroup, Label, Input, InputGroup, InputGroupAddon, InputGroupText, Row, ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.nameChangeCarrier = this.nameChangeCarrier.bind(this);
+    this.state = {
+      dropdownOpen: false,
+      value: "Please Select"
+    };
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+  nameChangeCarrier = (event) => {
+    this.setState({
+      dropdownOpen : !this.state.dropdownOpen,
+      value : event.target.innerText
+    });
+  }
+  
   render() {
+   
     return (
       <div className="app flex-row align-items-center">
         <Container>
           <Row className="justify-content-center">
-            <Col md="6">
-              <Card className="mx-4">
+            <Col md="6" className="Register">
+              <Card className="mx-4 RegisterPage">
                 <CardBody className="p-4">
                   <h1 className='text-center color-main'>Signup</h1>
                   <p className="text-center color-main">Create your account</p>
                   <FormGroup>
                     <Label htmlFor="full name">Full Name</Label>
-                    <Input type="text" id="full name" placeholder="full name" required />
+                    <Input type="text" id="full name" placeholder="Mihai Petrea" required />
                   </FormGroup>
                   <FormGroup>
                     <Label htmlFor="carrier">Carrier</Label>
-                    <Input type="text" id="carrier" placeholder="carrier" required />
+                    <div>
+                      <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                      <DropdownToggle caret size="lg" className="Carrier">
+                     {this.state.value} <FontAwesomeIcon icon="angle-down" className="angleDown"/>
+                      </DropdownToggle>
+                      <DropdownMenu className="carrierChoice">
+                        <DropdownItem onClick={this.nameChangeCarrier}>Softbank</DropdownItem>
+                        <DropdownItem onClick={this.nameChangeCarrier}>KDDI</DropdownItem>
+                        <DropdownItem onClick={this.nameChangeCarrier}>UQ</DropdownItem>
+                        <DropdownItem onClick={this.nameChangeCarrier}>DOKOMO</DropdownItem>
+                        <DropdownItem onClick={this.nameChangeCarrier}>R</DropdownItem>
+                      </DropdownMenu>
+                    </ButtonDropdown>
+                    </div>
+          
                   </FormGroup>
                   <FormGroup>
                     <Label htmlFor="email">Email</Label>
-                    <Input type="email" id="email" placeholder="email" required />
+                    <Input type="email" id="email" placeholder="user@domain.com" required />
                   </FormGroup>
                   <FormGroup>
                     <Label htmlFor="password">password</Label>
-                    <Input type="password" id="password" placeholder="" required />
+                    <Input type="password" id="password" placeholder="●●●●●●●" required />
                   </FormGroup>
                   <br/>
                   <Row className="justify-content-center">
@@ -37,17 +75,29 @@ class Register extends Component {
                       </Link>
                     </Col>
                   </Row>
-                  <br/>
-                  <Row className="justify-content-center">
-                    <Col md='auto'>
-                      Already have an account? 
-                      <Link to='/login'> Login!</Link>
-                    </Col>
-                  </Row>
                 </CardBody>
               </Card>
+              <Row className="justify-content-center Link-to-signin">
+                <Col md='auto'>
+                  Already have an account? 
+                  <Link to='/login'> Login!</Link>
+                </Col>
+              </Row>
             </Col>
           </Row>
+         
+    {/* <DropdownButton
+      bsSize="large"
+      title="Large button"
+      id="dropdown-size-large"
+    >
+      <MenuItem eventKey="1">Action</MenuItem>
+      <MenuItem eventKey="2">Another action</MenuItem>
+      <MenuItem eventKey="3">Something else here</MenuItem>
+      <MenuItem divider />
+      <MenuItem eventKey="4">Separated link</MenuItem>
+    </DropdownButton>
+   */}
         </Container>
       </div>
     );
