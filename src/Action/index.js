8235968f2e32297b02d7c4f9cfae302c2baa7136
nantapat <minibio_nan@hotmail.com>
2018-10-22 00:13:37 +0700

    Add some comments

--- src/Action/index.js
+++ src/Action/index.js
@@ -4,13 +4,13 @@ async function signUp (data){
     const response = await fetch(baseUrl+'mobacon/api/web/signup', {
         method: 'POST',
         headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
+            Accept: 'application/json', // เซิฟเวอร์จะตอบกลับมารูปแบบไหน
+            'Content-Type': 'application/json' // สิ่งที่จะส่งไปให้เซิฟเวอร์รูปแบบไหน
         },
-        body: JSON.stringify(data),
-    }).then(resp => resp.json());
+        body: JSON.stringify(data), //เอาดาต้าที่ได้ไปแปลงเป็นเจสันก่อน
+    }).then(resp => resp.json()); //แปลงเรสปอนซ์ที่ได้กลับมาให้กลายเป็นไฟล์เจสัน
     console.log(response);
-    return response;
+    return response; 
 }
 
 export default { signUp };
