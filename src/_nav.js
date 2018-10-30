9258d1a22ca48ce408062c593c4ce4030913215c
nantapat <minibio_nan@hotmail.com>
2018-10-30 18:40:47 +0700

    Adjust sidebar ui

--- src/_nav.js
+++ src/_nav.js
@@ -1,7 +1,9 @@
+
 export default {
   items: [
     {
-      name: 'Dashboard',
+      icon: 'icon-chart',
+      name: 'DASHBOARD',
       url: '/dashboard',
       // icon: 'icon-speedometer',
       // badge: {
@@ -10,38 +12,45 @@ export default {
       // },
     },
     {
-      name: 'Plans',
+      name: 'PLANS',
       url: '/plans',
+      icon: 'icon-list',
       // icon: 'icon-speedometer',
     },
     {
-      name: 'Offers',
+      name: 'OFFERS',
       url: '/offers',
+      icon: 'icon-tag'
       // icon: 'icon-speedometer',
     },
     {
-      name: 'Requests',
+      name: 'REQUESTS',
       url: '/requests',
+      icon: 'icon-envelope'
       // icon: 'icon-speedometer',
     },
     {
-      name: 'Operator (Contract)',
+      name: 'OPERATOR (CONTRACT)',
       url: '/operator',
+      icon: 'icon-user'
       // icon: 'icon-speedometer',
     },
     {
-      name: 'Chat',
+      name: 'CHAT',
       url: '/chat',
+      icon: 'icon-bubble'
       // icon: 'icon-speedometer',
     },
     {
       name: 'Your Profile',
       url: '/profile',
+      icon: 'icon-settings'
       // icon: 'icon-speedometer',
     },
     {
       name: 'Logout',
       url: '/logout',
+      icon: 'icon-logout'
       // icon: 'icon-speedometer',
     },
 
