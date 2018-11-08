cde02de71064e2378f487effca8adb144f922c38
nantapat <minibio_nan@hotmail.com>
2018-11-08 15:46:34 +0700

    Adjust UI of single request page

--- src/pages/Request/index.js
+++ src/pages/Request/index.js
@@ -14,8 +14,8 @@ import {
   ListGroupItem,
   Table,
   Badge,
-
 } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import RequestAside from './Aside';
 import _ from 'lodash';
 
@@ -38,9 +38,13 @@ class Tabs extends Component {
     return (
       <div className="animated fadeIn">
         <Row>
-          <Col xs="12" md="12" className="mb-4">
+          <Col xs="6" md="6" className="mb-4" className="alignHeader">
+            <p className="alignRequestIndex">Mihai Petrea's Request</p>
+            <div className="floatLeft"><span className="pending_index">Pending</span></div>
+          </Col>
+          <Col xs="6" md="6" className="mb-4" className="alignHeader">
             <div style={{float: 'right'}}>
-              <Button type="submit" color="primary" onClick={() => this.setState({aside: true})}>VIEW HISTORY</Button>
+              <Button type="submit" className="adjustButtonHistory" onClick={() => this.setState({aside: true})}>VIEW HISTORY</Button>
               <RequestAside visible={this.state.aside}/>
             </div>
           </Col>          
@@ -49,58 +53,65 @@ class Tabs extends Component {
           <Col md='6'>
             <Card>
               <CardBody>
-                <Form>
-                  <FormGroup>
-                    <Label htmlFor="SMS">Your Review</Label>
+                <Form className="requestFormIndex">
+                  <FormGroup className="alignFormGroup" >
+                    <Label htmlFor="SMS" className="label">Your Review</Label>
                     <Input type="textarea" name="textarea-input" id="textarea-input" rows="6"
-                      placeholder="Write your review..." />
+                      placeholder="Write your review" className="textArea" />
                   </FormGroup>
-                  <FormGroup>
-                    <Label htmlFor="request">Request</Label><br/>
+                  <FormGroup className="alignFormGroupStatus">
+                    <Label htmlFor="request" className="label">Request Status</Label><br/>
                     <FormGroup check inline>
                       <Input className="form-check-input" type="radio" id="inline-radio1" name="inline-radios" value="option1" />
-                      <Label className="form-check-label" check htmlFor="inline-radio1">Accept</Label>
+                      <label className="form-check-label" check htmlFor="inline-radio1" className="enableAccept">
+                        <FontAwesomeIcon icon="circle" className="faCircle-1" ></FontAwesomeIcon>
+                        Accept
+                      </label>
                     </FormGroup>
                     <FormGroup check inline>
                       <Input className="form-check-input" type="radio" id="inline-radio2" name="inline-radios" value="option2" />
-                      <Label className="form-check-label" check htmlFor="inline-radio2">Reject</Label>
+                      <Label className="form-check-label" check htmlFor="inline-radio2" className="enableDecline">
+                      <FontAwesomeIcon icon="circle" className="faCircle-2" ></FontAwesomeIcon>
+                        Decline
+                      </Label>
+                      
                     </FormGroup>
                   </FormGroup>
-                  <Button type="submit" color="primary">SUBMIT</Button>
+                  <Button type="submit" className="adjustSubmitButton">SUBMIT</Button>
                 </Form>
               </CardBody>
             </Card>
           </Col>
           <Col md='6'>
-            <Card>
-              <ListGroup>
-                <ListGroupItem>
-                  <div style={{float: 'left'}}>CLIENT SINCE</div>
-                  <div style={{float: 'right'}}>CLIENT SINCE</div>
+            <Card className="asideIndex">
+              <ListGroup className="increaseHeight">
+                <ListGroupItem className="adjustListgroupHead">
+                  <div style={{float: 'left'}} className="textLeft">CLIENT SINCE</div>
+                  <div style={{float: 'right'}} className="textRight">CLIENT SINCE</div>
                 </ListGroupItem>
-                <ListGroupItem>
-                  <div style={{float: 'left'}}>ACTIVE PLAN</div>
-                  <div style={{float: 'right'}}>Basic Plan</div>
+                <ListGroupItem className="adjustListgroup">
+                  <div style={{float: 'left'}} className="textLeft">ACTIVE PLAN</div>
+                  <div style={{float: 'right'}} className="textRight">Basic Plan</div>
                 </ListGroupItem>
-                <ListGroupItem>
-                  <div style={{float: 'left'}}>FAMILY</div>
-                  <div style={{float: 'right'}}>Yes</div>
+                <ListGroupItem className="adjustListgroup">
+                  <div style={{float: 'left'}} className="textLeft">FAMILY</div>
+                  <div style={{float: 'right'}} className="textRight">Yes</div>
                 </ListGroupItem>
-                <ListGroupItem>
-                  <div style={{float: 'left'}}>DATE REQUESTED</div>
-                  <div style={{float: 'right'}}>17.06.2018</div>
+                <ListGroupItem className="adjustListgroup">
+                  <div style={{float: 'left'}} className="textLeft">DATE REQUESTED</div>
+                  <div style={{float: 'right'}} className="textRight">17.06.2018</div>
                 </ListGroupItem>
-                <ListGroupItem>
-                  <div style={{float: 'left'}}>OPERATOR</div>
-                  <div style={{float: 'right'}}>Chuck Norris</div>
+                <ListGroupItem className="adjustListgroup">
+                  <div style={{float: 'left'}} className="textLeft">OPERATOR</div>
+                  <div style={{float: 'right'}} className="textRight">Chuck Norris</div>
                 </ListGroupItem>
-                <ListGroupItem>
-                  <div style={{float: 'left'}}>AVERAGE BILL</div>
-                  <div style={{float: 'right'}}>200$</div>
+                <ListGroupItem className="adjustListgroup">
+                  <div style={{float: 'left'}} className="textLeft">AVERAGE BILL</div>
+                  <div style={{float: 'right'}} className="textRight">200$</div>
                 </ListGroupItem>
-                <ListGroupItem>
-                  <div style={{float: 'left'}}>FEEDBACK</div>
-                  <div style={{float: 'right'}}><i className='fa fa-thumbs-o-up'></i> Liked Offer</div>
+                <ListGroupItem className="adjustListgroupTail">
+                  <div style={{float: 'left'}} className="textLeft">FEEDBACK</div>
+                  <div style={{float: 'right'}} className="textRightOffer">Liked Offer</div>
                 </ListGroupItem>
               </ListGroup>
             </Card>
@@ -110,49 +121,68 @@ class Tabs extends Component {
         <Row>
           <Col md='12'>
             <Card>
-              <CardHeader>
+              <CardHeader className="billText">
                 Bill History
               </CardHeader>
-              <CardBody>
-                <Table responsive>
-                  <thead>
-                  <tr>
-                    <th>MONTH</th>
-                    <th>AMOUNT</th>
-                    <th>MINUTES USED</th>
-                    <th>SMS USED</th>
-                    <th>INTERNET USED</th>
-                    <th>EMISSION DATE</th>
-                    <th>PAID DATE</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  {_.range(10).map(i => 
+              <CardBody className="cardBodyBill">
+                <div className="alignTable">
+                  <Table responsive className="tableRequest_1">
+                    <thead className="thead">
                     <tr>
-                      <td>July</td>
-                      <td>2200$</td>
-                      <td>10 / 100</td>
-                      <td>10 / 100</td>
-                      <td>2GB / 4GB</td>
-                      <td>01.07.2018</td>
-                      <td>UNPAID</td>
+                      <th>MONTH</th>
+                      <th>AMOUNT</th>
+                      <th>MINUTES USED</th>
+                      <th>SMS USED</th>
+                      <th>INTERNET USED</th>
+                      <th>EMISSION DATE</th>
+                      <th>PAID DATE</th>
                     </tr>
-                  )}
-                  </tbody>
-                </Table>
+                    </thead>
+                    <tbody className="tbody">
+                      <tr>
+                        <td>August</td>
+                        <td>$230</td>
+                        <td>2341/3000</td>
+                        <td>1432/2000</td>
+                        <td>2GB / 4GB</td>
+                        <td>01.08.2018</td>
+                        <td>07.08.2018</td>
+                      </tr>
+                      <tr>
+                        <td>July</td>
+                        <td>$230</td>
+                        <td>2341/3000</td>
+                        <td>1432/2000</td>
+                        <td>2GB / 4GB</td>
+                        <td>01.08.2018</td>
+                        <td>07.08.2018</td>
+                      </tr>
+                      <tr>
+                        <td>June</td>
+                        <td>$230</td>
+                        <td>2341/3000</td>
+                        <td>1432/2000</td>
+                        <td>2GB / 4GB</td>
+                        <td>01.08.2018</td>
+                        <td>07.08.2018</td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                  <div className="fullHistory">SEE FULL HISTORY</div>
+               </div>
               </CardBody>
             </Card>
           </Col>
         </Row>
         <Card>
           <CardBody>
-            <Form>
-              <FormGroup>
-                <Label htmlFor="memos">Memos (for internal use only)</Label>
+            <Form className="memoFromIndex">
+              <FormGroup className="alignFormGroup">
+                <Label htmlFor="memos" className="label">Memos (for internal use only)</Label>
                 <Input type="textarea" name="textarea-input" id="textarea-input" rows="8"
-                  placeholder="Write your memo" />
+                  placeholder="Write your memo" className="textAreaMemo" />
               </FormGroup>
-              <Button type="submit" color="primary">SAVE</Button>
+              <Button type="submit" className="adjustSaveButton">SAVE</Button>
             </Form>
           </CardBody>
         </Card>
