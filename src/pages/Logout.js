183843f6152461457e19288153e56c1fb984a3c9
nantapat <minibio_nan@hotmail.com>
2018-11-19 17:49:09 +0700

    Clear localstorage

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 
 class Logout extends Component {
+  componentDidMount = () => {
+    localStorage.clear();
+  }
   render() {
     return (
       <Redirect to='/login'/>
